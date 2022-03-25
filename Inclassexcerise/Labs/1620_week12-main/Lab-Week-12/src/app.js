a81752510b15ae11413026a75d29ac45133cd61f
Harkaran <kang.harkaran21@gmail.com>
2022-03-25 12:51:49 -0700

    Edited my old code for grade average to use a function for part 3

--- Inclassexcerise/Labs/1620_week12-main/Lab-Week-12/src/app.js
+++ Inclassexcerise/Labs/1620_week12-main/Lab-Week-12/src/app.js
@@ -6,6 +6,26 @@ function elementlogger() {
 }
 console.log(elementlogger())
 
+//Part 3
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+  
+  const studentsScholarship = []
+  
+   for (const student of students) {
+        let average = student.grades.reduce((x,y) => x+y,0) / student.grades.length;
+      if(average >= 80){
+      const ob = [{name: student.name, gradeAvg: average}]
+      studentsScholarship.push(ob)
+      }
+    }
+  
+  console.log(studentsScholarship)
+
 
 
 
